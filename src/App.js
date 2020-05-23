20fdac56e42c00effbf19b9ba22e3120e1cc758b
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-05-23 22:04:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+        <div className="inner_wrapper">Hi</div>
+    </div>
+  );
+}
+
+export default App;
